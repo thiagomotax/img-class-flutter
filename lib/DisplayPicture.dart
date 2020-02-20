e7f5fedd639fef5fc8caca5e4d01faef7af948ea
Thiago Mota <thiagomotax@gmail.com>
2020-02-20 11:20:54 -0300

    transicao pro repositorio

---
+++ lib/DisplayPicture.dart
@@ -0,0 +1,55 @@
+// A widget that displays the picture taken by the user.
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Imagem capturada')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Image.file(File(imagePath)),
+          ),
+          Container(
+            child: RaisedButton(
+              color: Colors.orange,
+              onPressed: cortarImagem,
+              child: Text("Cortar"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  cortarImagem() async {
+    File croppedFile = await ImageCropper.cropImage(
+        sourcePath: imagePath,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          minimumAspectRatio: 1.0,
+        ));
+  }
+}
